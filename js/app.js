6bfbc69372450f9b2c74ee9047fc8bec33012fac
Ben Chapman <bchapman@engineyard.com>
2013-05-30 02:03:02 +0100

    Hacked together geocoding

--- js/app.js
+++ js/app.js
@@ -1,9 +1,54 @@
-console.log(
-	geolib.findNearest(
-		{
-			latitude: 53.3498053,
-			longitude: -6.2603097
-		},
-		data
-	)
-);
+var map;
+var markers = [];
+var geocoder;
+
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var mapOptions = {
+    center: new google.maps.LatLng(25,0),
+    zoom: 2,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+	streetViewControl: false
+  };
+  map = new google.maps.Map(document.getElementById("map-box"), mapOptions);
+  for(var i in data) {
+	  markers[i] = new google.maps.Marker({
+	      position: new google.maps.LatLng(data[i].latitude,data[i].longitude),
+	      map: map,
+	      title: i,
+		  clickable: true,
+		  icon: {
+			  path: google.maps.SymbolPath.CIRCLE,
+			  fillColor: 'ff3333',
+			  fillOpacity: 1,
+			  scale: 4,
+			  strokeOpacity: 1,
+			  strokeWeight: 1
+		  }
+	  });
+  }
+}
+function codeAddress(myLocation) {
+  var address = myLocation;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+	  var closest = geolib.findNearest({
+		  latitude:results[0].geometry.location.jb,
+		  longitude: results[0].geometry.location.kb
+	  },data);
+	  console.log(closest);
+      map.setCenter(new google.maps.LatLng(closest.latitude,closest.longitude));
+	  map.setZoom(12);
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+window.addEventListener('load',function() {
+	document.getElementById("location").addEventListener('change',function(){
+		myLocation = this.value;
+		codeAddress(myLocation)
+	})
+});
--- js/data.js
+++ js/data.js
@@ -1,12 +1,487 @@
 var data = {
-	"Science Gallery, Dublin": {
-		id: 27,
-		latitude: 53.344199,
-		longitude: -6.250057
-	},
-	"Github, San Francisco": {
-		id: 25,
-		latitude: 37.778907,
-		longitude: -122.397426
-	}
+    "NSC Mahon Cork": {
+        "id": 23,
+        "latitude": 51.887511,
+        "longitude": -8.405507
+    },
+    "GitHub, San Francisco": {
+        "id": 25,
+        "latitude": 37.778907,
+        "longitude": -122.397426
+    },
+    "Dublin @ Science Gallery": {
+        "id": 27,
+        "latitude": 53.344199,
+        "longitude": -6.250057
+    },
+    "Limerick": {
+        "id": 32,
+        "latitude": 52.666870636661,
+        "longitude": -8.6218750476837
+    },
+    "Den Haag": {
+        "id": 33,
+        "latitude": 52.0698,
+        "longitude": 4.33694
+    },
+    "Athlone": {
+        "id": 34,
+        "latitude": 53.419469090755,
+        "longitude": -7.9069590568542
+    },
+    "Indianapolis": {
+        "id": 38,
+        "latitude": 39.778539,
+        "longitude": -86.13221
+    },
+    "WicklowTown": {
+        "id": 41,
+        "latitude": 52.985457,
+        "longitude": -6.046557
+    },
+    "Dublin D15": {
+        "id": 44,
+        "latitude": 53.405617,
+        "longitude": -6.378057
+    },
+    "Castlebar": {
+        "id": 49,
+        "latitude": 53.857516,
+        "longitude": -9.308018
+    },
+    "Maynooth": {
+        "id": 50,
+        "latitude": 53.382790822565,
+        "longitude": -6.6023433208466
+    },
+    "CoderDojo Bandung": {
+        "id": 51,
+        "latitude": -6.890139,
+        "longitude": 107.617248
+    },
+    "Athenry": {
+        "id": 53,
+        "latitude": 53.301094808878,
+        "longitude": -8.7453103065491
+    },
+    "Tokyo": {
+        "id": 57,
+        "latitude": 35.665511951241,
+        "longitude": 139.663438797
+    },
+    "Drogheda on the Boyne": {
+        "id": 60,
+        "latitude": 53.715396,
+        "longitude": -6.348705
+    },
+    "Des Moines": {
+        "id": 62,
+        "latitude": 41.542101,
+        "longitude": -93.789596
+    },
+    "CavanTown": {
+        "id": 63,
+        "latitude": 54.000858,
+        "longitude": -7.35702
+    },
+    "Thurles": {
+        "id": 65,
+        "latitude": 52.686139,
+        "longitude": -7.826085
+    },
+    "Iowa City": {
+        "id": 68,
+        "latitude": 41.659261,
+        "longitude": -91.535425
+    },
+    "Arainn Mhor Island": {
+        "id": 70,
+        "latitude": 54.98538,
+        "longitude": -8.49202
+    },
+    "Bray": {
+        "id": 71,
+        "latitude": 53.19180319979,
+        "longitude": -6.10227227211
+    },
+    "Remark Media Atlanta": {
+        "id": 73,
+        "latitude": 33.917242,
+        "longitude": -84.356235
+    },
+    "Forward Technology, London NW1 0DU": {
+        "id": 74,
+        "latitude": 51.537757,
+        "longitude": -0.136774
+    },
+    "Claremorris": {
+        "id": 75,
+        "latitude": 53.71975,
+        "longitude": -8.997
+    },
+    "London Hackspace, London E2 8HD": {
+        "id": 76,
+        "latitude": 51.530538,
+        "longitude": -0.07617
+    },
+    "Twitter, San Francisco": {
+        "id": 81,
+        "latitude": 37.775973,
+        "longitude": -122.416792
+    },
+    "Waterford": {
+        "id": 83,
+        "latitude": 52.253854,
+        "longitude": -7.187805
+    },
+    "Dublin North - DCU": {
+        "id": 89,
+        "latitude": 53.389881,
+        "longitude": -6.251135
+    },
+    "The AmmA Centre, Armagh": {
+        "id": 90,
+        "latitude": 54.347909,
+        "longitude": -6.654078
+    },
+    "Munich": {
+        "id": 92,
+        "latitude": 48.163509,
+        "longitude": 11.558261
+    },
+    "Milano": {
+        "id": 95,
+        "latitude": 45.497684,
+        "longitude": 9.221263
+    },
+    "BraveNewTalent, London Holborn, WC2": {
+        "id": 96,
+        "latitude": 51.5163304,
+        "longitude": -0.1207678
+    },
+    "Wuerzburg": {
+        "id": 97,
+        "latitude": 49.811191086564,
+        "longitude": 9.8892107605934
+    },
+    "CPS-Coder": {
+        "id": 100,
+        "latitude": -22.814652,
+        "longitude": -47.064292
+    },
+    "Glasgow, Scotland": {
+        "id": 102,
+        "latitude": 55.858502,
+        "longitude": -4.29368
+    },
+    "Charleston Dojo - Jack Russell Software": {
+        "id": 107,
+        "latitude": 32.841231,
+        "longitude": -79.867948
+    },
+    "Belfast": {
+        "id": 110,
+        "latitude": 54.60726,
+        "longitude": -5.909293
+    },
+    "Cape Town": {
+        "id": 113,
+        "latitude": -33.891971,
+        "longitude": 18.505461
+    },
+    "Cork CIT Blackrock Castle Observatory": {
+        "id": 118,
+        "latitude": 51.89988,
+        "longitude": -8.40287
+    },
+    "Dundalk": {
+        "id": 128,
+        "latitude": 53.984459,
+        "longitude": -6.393785
+    },
+    "Derry": {
+        "id": 135,
+        "latitude": 55.006271551287,
+        "longitude": -7.322301864624
+    },
+    "Enniscorthy": {
+        "id": 142,
+        "latitude": 52.511422,
+        "longitude": -6.583745
+    },
+    "JaxCoderDojo": {
+        "id": 143,
+        "latitude": 30.330389,
+        "longitude": -81.646866
+    },
+    "Goshen": {
+        "id": 147,
+        "latitude": 41.584858,
+        "longitude": -85.834618
+    },
+    "Silicon Valley CoderDojo": {
+        "id": 159,
+        "latitude": 37.410528,
+        "longitude": -122.071195
+    },
+    "Hibarigaoka": {
+        "id": 162,
+        "latitude": 35.756262,
+        "longitude": 139.549232
+    },
+    "Euskal Herriko Dojo": {
+        "id": 163,
+        "latitude": 43.412569,
+        "longitude": -2.929135
+    },
+    "Okinawa": {
+        "id": 165,
+        "latitude": 26.200499,
+        "longitude": 127.688148
+    },
+    "Parlarie": {
+        "id": 166,
+        "latitude": 52.389208,
+        "longitude": 4.889
+    },
+    "Birr, Co. Offaly": {
+        "id": 167,
+        "latitude": 53.092696837367,
+        "longitude": -7.9113149642944
+    },
+    "Asahikawa": {
+        "id": 170,
+        "latitude": 44.042230902538,
+        "longitude": 142.364819
+    },
+    "Melbourne HackerSpace": {
+        "id": 172,
+        "latitude": -37.823478,
+        "longitude": 145.038776
+    },
+    "Mullingar": {
+        "id": 175,
+        "latitude": 53.514182925866,
+        "longitude": -7.3717617988586
+    },
+    "Lund": {
+        "id": 187,
+        "latitude": 55.707546,
+        "longitude": 13.21439
+    },
+    "Asahikawa-city": {
+        "id": 201,
+        "latitude": 44.038244294235,
+        "longitude": 142.3649597168
+    },
+    "CoderDojo Navan": {
+        "id": 207,
+        "latitude": 53.652899,
+        "longitude": -6.679442
+    },
+    "CoderDojo Doon": {
+        "id": 213,
+        "latitude": 52.606716,
+        "longitude": -8.243364
+    },
+    "CoderDojo STORM Saint Gratien 95": {
+        "id": 215,
+        "latitude": 48.961849,
+        "longitude": 2.286412
+    },
+    "Shiojiri": {
+        "id": 217,
+        "latitude": 36.112033,
+        "longitude": 137.95283
+    },
+    "Kushimoto": {
+        "id": 220,
+        "latitude": 33.47247,
+        "longitude": 135.781498
+    },
+    "UCC Coder Dojo": {
+        "id": 229,
+        "latitude": 51.892985,
+        "longitude": -8.49009
+    },
+    "Cork Internet eXchange": {
+        "id": 232,
+        "latitude": 51.903564,
+        "longitude": -8.512341
+    },
+    "IBM CoderDojo": {
+        "id": 233,
+        "latitude": 53.421195,
+        "longitude": -6.419449
+    },
+    "avego cork": {
+        "id": 238,
+        "latitude": 51.901069,
+        "longitude": -8.46407
+    },
+    "Clockwork, Minneapolis (CoderDojo Twin Cities)": {
+        "id": 239,
+        "latitude": 44.99166,
+        "longitude": -93.230313
+    },
+    "Okayama": {
+        "id": 243,
+        "latitude": 34.668494,
+        "longitude": 133.916398
+    },
+    "Sendai": {
+        "id": 244,
+        "latitude": 38.26264,
+        "longitude": 140.876474
+    },
+    "Killorglin": {
+        "id": 246,
+        "latitude": 52.105477,
+        "longitude": -9.788135
+    },
+    "Dungarvan": {
+        "id": 250,
+        "latitude": 52.08486,
+        "longitude": -7.62198
+    },
+    "Salzburg": {
+        "id": 252,
+        "latitude": 47.822990459856,
+        "longitude": 13.040417432785
+    },
+    "medialab-prado Madrid": {
+        "id": 255,
+        "latitude": 40.410669591941,
+        "longitude": -3.6936163902283
+    },
+    "North Dakota": {
+        "id": 263,
+        "latitude": 46.810103,
+        "longitude": -100.783778
+    },
+    "Kudos Dojo": {
+        "id": 264,
+        "latitude": -17.376509548081,
+        "longitude": -66.165407896042
+    },
+    "CoderDojo Ballymena": {
+        "id": 274,
+        "latitude": 54.864827,
+        "longitude": -6.27769
+    },
+    "Belfast - Farset Labs": {
+        "id": 279,
+        "latitude": 54.592866,
+        "longitude": -5.940728
+    },
+    "Barcelona-CiutatVella": {
+        "id": 283,
+        "latitude": 41.3786,
+        "longitude": 2.1646
+    },
+    "Brescia": {
+        "id": 285,
+        "latitude": 45.526074,
+        "longitude": 10.211449
+    },
+    "Elimo 0-1": {
+        "id": 289,
+        "latitude": 38.075133083238,
+        "longitude": 12.679429650307
+    },
+    "CoderDojo Bilbao": {
+        "id": 294,
+        "latitude": 43.271909,
+        "longitude": -2.938735
+    },
+    "Zambrow": {
+        "id": 296,
+        "latitude": 52.98605,
+        "longitude": 22.24718
+    },
+    "Miltown Malbay": {
+        "id": 299,
+        "latitude": 52.846792,
+        "longitude": -9.431687
+    },
+    "CoderDojo Eureka@Bangalore": {
+        "id": 308,
+        "latitude": 12.849116,
+        "longitude": 77.681133
+    },
+    "Bologna": {
+        "id": 312,
+        "latitude": 44.494592,
+        "longitude": 11.342396
+    },
+    "Portland, OR": {
+        "id": 324,
+        "latitude": 45.519495755123,
+        "longitude": -122.68278121948
+    },
+    "Westport Coderdojo": {
+        "id": 325,
+        "latitude": 53.79943422,
+        "longitude": -9.518103
+    },
+    "Malahide": {
+        "id": 329,
+        "latitude": 53.450072,
+        "longitude": -6.153502
+    },
+    "CDYS Mallow": {
+        "id": 343,
+        "latitude": 52.139183,
+        "longitude": -8.642509
+    },
+    "CoderDojo Salinas": {
+        "id": 345,
+        "latitude": 36.672188,
+        "longitude": -121.603893
+    },
+    "Manchester": {
+        "id": 347,
+        "latitude": 53.484164,
+        "longitude": -2.236297
+    },
+    "CoderDojo LX": {
+        "id": 358,
+        "latitude": 38.760037,
+        "longitude": -9.193682
+    },
+    "Huntsville, AL": {
+        "id": 362,
+        "latitude": 34.724098153827,
+        "longitude": -86.678612530231
+    },
+    "Torino": {
+        "id": 365,
+        "latitude": 45.069368,
+        "longitude": 7.701244
+    },
+    "Omagh": {
+        "id": 366,
+        "latitude": 54.60163,
+        "longitude": -7.302282
+    },
+    "Aberdeen": {
+        "id": 367,
+        "latitude": 57.165311,
+        "longitude": -2.10448
+    },
+    "Tallaght": {
+        "id": 372,
+        "latitude": 53.28842,
+        "longitude": -6.373101
+    },
+    "Carrick on Suir": {
+        "id": 376,
+        "latitude": 52.349369,
+        "longitude": -7.410413
+    },
+    "Pembroke Pines": {
+        "id": 378,
+        "latitude": 26.031022,
+        "longitude": -80.373128
+    }
 }
